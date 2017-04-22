410aa792aecb5dbfd5fbce6ae901d8640f81fde6
dominikgrabinski <grabinski.dominik@gmail.com>
2017-04-22 10:48:45 +0200

    ex with DB

---
+++ projekt_view/src/WidokTwigBundle/Entity/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace WidokTwigBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+    /**
+     * @ORM\Entity
+     * @ORM\Table(name = "posts")
+     */
+
+class Post {
+    
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue 
+     */
+    private $id;
+    
+    /**
+     *  @ORM\Column(type="string", length = 100)
+     */
+    
+    private $title;
+    private $text;
+    
+
+}
